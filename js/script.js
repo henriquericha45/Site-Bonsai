870d1e1b90450559b40cd05cd9819eb0e8618ffc
henriquericha45 <henriquericha@hotmail.com>
2021-11-02 12:09:33 -0300

    correcoes trabalho

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+var maisVendidos = [[0, 'Jabuticaba Sabará', 'bonsai1.jpeg', false],
+                    [1, 'Pyracantha', 'bonsai2.jpeg', false],
+                    [2, 'Bougainville', 'bonsai3.jpeg', false]];
+
 var listaProdutos = [[0, 'Jabuticaba Sabará', 'bonsai1.jpeg', false],
                      [1, 'Pyracantha', 'bonsai2.jpeg', false],
                      [2, 'Bougainville', 'bonsai3.jpeg', false], 
@@ -16,20 +20,20 @@ function montarCardProdutos(){
     document.getElementById("divProdutos").innerHTML = "";
 
 
-    for(var i = 0; i < listaProdutos.length; i++)
+    for(var i = 0; i < maisVendidos.length; i++)
     {
         var conteudo = "";
         conteudo += '<div class="div-card" >';
         conteudo += '<div class="div-card-img">';
-        conteudo += '<img src="imagens/' + listaProdutos[i][2] + '" />';
+        conteudo += '<img src="imagens/' + maisVendidos[i][2] + '" />';
         conteudo += '</div>';
         conteudo += '<div class="div-card-descricao">';
-        conteudo += listaProdutos[i][1];
+        conteudo += maisVendidos[i][1];
         conteudo += '</div>';
 
-        if(listaProdutos[i][3] == false)
+        if(maisVendidos[i][3] == false)
         {
-            conteudo += '<div class="div-card-comprar"  onclick="comprar(' + listaProdutos[i][0] + ')"  >';
+            conteudo += '<div class="div-card-comprar"  onclick="comprar(' + maisVendidos[i][0] + ')"  >';
             conteudo += 'Comprar';
             conteudo += '</div>';
         }
@@ -50,9 +54,9 @@ function montarCardProdutos(){
 
 function comprar(id){
 
-    listaProdutos[id][3] = true;
+    maisVendidos[id][3] = true;
     
-    carrinho.push(listaProdutos[id]);
+    carrinho.push(maisVendidos[id]);
 
     window.localStorage.setItem("carrinho", JSON.stringify(carrinho));
 
